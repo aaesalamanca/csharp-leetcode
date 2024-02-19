df9ff6e7bcb10b3a43d849ff4794fcdd2aabe2d9
aaesalamanca <aaesalamanca@gmail.com>
2024-02-19 17:26:00 +0100

    feat: Third solution (with division) to Problem 9 Palindrome Number

---
+++ test/Problem0009.PalindromeNumber.Library.UnitTest/WithDivisionTest.cs
@@ -0,0 +1,23 @@
+using Problem0009.PalindromeNumber.Library.Implementations;
+using Problem0009.PalindromeNumber.Library.Interfaces;
+
+namespace Problem0009.PalindromeNumber.Library.UnitTest;
+
+public class WithDivisionTest
+{
+    [Theory]
+    [InlineData(121, true)]
+    [InlineData(-121, false)]
+    [InlineData(10, false)]
+    public void IsPalindrome_ReturnsIfIsPalindrome(int x, bool expected)
+    {
+        // Arrange.
+        ISolution solution = new WithDivision();
+
+        // Act.
+        bool actual = solution.IsPalindrome(x);
+
+        // Assert.
+        Assert.Equal(expected, actual);
+    }
+}
