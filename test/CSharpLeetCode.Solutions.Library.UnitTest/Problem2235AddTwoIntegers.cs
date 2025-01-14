ec4af2800253a3d60b60b8e37b0ca272c312827a
aaesalamanca <aaesalamanca@gmail.com>
2025-01-14 17:12:50 +0100

    test: Check the Add Two Integers solution

---
+++ test/CSharpLeetCode.Solutions.Library.UnitTest/Problem2235AddTwoIntegers.cs
@@ -0,0 +1,19 @@
+namespace CSharpLeetCode.Solutions.Library.UnitTest;
+
+public class Problem2235AddTwoIntegers
+{
+    [Theory]
+    [InlineData(12, 5, 17)]
+    [InlineData(-10, 4, -6)]
+    public void AddTwoIntegers_ReturnsTarget(int num1, int num2, int expected)
+    {
+        // Arrange.
+        IProblem2235AddTwoIntegers sut = new Problem2235AddTwoIntegers();
+
+        // Act.
+        var actual = sut.Sum(num1, num2);
+
+        // Assert.
+        Assert.Equal(expected, actual);
+    }
+}
