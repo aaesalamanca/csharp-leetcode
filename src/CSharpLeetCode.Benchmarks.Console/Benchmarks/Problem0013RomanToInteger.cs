31d81b8d58f5f70cf8f609adc870d3173f6841ac
aaesalamanca <aaesalamanca@gmail.com>
2025-01-17 12:13:23 +0100

    perf: Benchmark Problem 0013 Roman To Integer

---
+++ src/CSharpLeetCode.Benchmarks.Console/Benchmarks/Problem0013RomanToInteger.cs
@@ -0,0 +1,22 @@
+using BenchmarkDotNet.Attributes;
+using Solution = CSharpLeetCode.Solutions.Library.Implementations.Problem0013RomanToInteger;
+
+namespace CSharpLeetCode.Benchmarks.Console.Benchmarks;
+
+[MemoryDiagnoser]
+public class Problem0013RomanToInteger
+{
+    private readonly Solution _problem0013RomanToInteger = new();
+
+    // Uncomment the data set to run the benchmark againts
+    public IEnumerable<object[]> Data()
+    {
+        yield return new object[] { "III" };
+        // yield return new object[] { "LVIII" };
+        // yield return new object[] { "MCMXCIV" };
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public int Solution(string s) => _problem0013RomanToInteger.RomanToInt(s);
+}
