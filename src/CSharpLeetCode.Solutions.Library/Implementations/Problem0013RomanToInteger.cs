5453b2dd1e7366ebfb5f7ba19a0293b2c85387ad
Alejandro Estornell <aaesalamanca@gmail.com>
2025-01-17 12:15:12 +0100

    feat: Problem 0013 Roman To Integer (#5)
    * test: Check Problem 0013 Roman To Integer data set
    
    * feat: Solve Problem 0013 Roman To Integer
    
    * perf: Benchmark Problem 0013 Roman To Integer

---
+++ src/CSharpLeetCode.Solutions.Library/Implementations/Problem0013RomanToInteger.cs
@@ -0,0 +1,48 @@
+using CSharpLeetCode.Solutions.Library.Interfaces;
+
+namespace CSharpLeetCode.Solutions.Library.Implementations;
+
+public class Problem0013RomanToInteger : IProblem0013RomanToInteger
+{
+    public int RomanToInt(string s)
+    {
+        var romanInt = RomanToIntArray(s);
+        var sum = 0;
+        for (int i = 0; i < romanInt.Length - 1; i++)
+        {
+            if (romanInt[i] >= romanInt[i + 1])
+            {
+                sum += romanInt[i];
+            }
+            else
+            {
+                sum -= romanInt[i];
+            }
+        }
+
+        sum += romanInt[^1];
+
+        return sum;
+    }
+
+    int[] RomanToIntArray(string s)
+    {
+        var romanInt = new int[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            romanInt[i] = s[i] switch
+            {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => 0,
+            };
+        }
+
+        return romanInt;
+    }
+}
