db6ec0a2721443276874bc0c8dc930534e286f2f
aaesalamanca <aaesalamanca@gmail.com>
2025-01-21 21:20:25 +0100

    perf: Benchmark Problem 0020 Valid Parentheses

---
+++ src/CSharpLeetCode.Benchmarks.Console/Benchmarks/Problem0020ValidParentheses.cs
@@ -0,0 +1,26 @@
+using BenchmarkDotNet.Attributes;
+using Solution = CSharpLeetCode.Solutions.Library.Implementations.Problem0020ValidParentheses;
+
+namespace CSharpLeetCode.Benchmarks.Console.Benchmarks;
+
+[MemoryDiagnoser]
+public class Problem0020ValidParentheses
+{
+    private readonly Solution _problem0020ValidParentheses = new();
+
+    // Uncomment the data set to run the benchmark against.
+    public IEnumerable<string> Data()
+    {
+        // yield return "()";
+        yield return "()[]{}";
+        // yield return "(]";
+        // yield return "([])";
+        // yield return "]";
+        // yield return "){";
+        // yield return "(])";
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public bool Solution(string s) => _problem0020ValidParentheses.IsValid(s);
+}
