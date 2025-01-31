47a5006e91d629aa0cdc29769833cc6cbc9470f6
Alejandro Estornell <aaesalamanca@gmail.com>
2025-01-31 16:45:10 +0100

    refactor: Review array initialization (#12)
    * refactor: Use implicit array initialization
    
    * refactor: Force implicit types

--- test/LeetSharp.Solutions.Library.UnitTest/Problem0001TwoSum.cs
+++ test/LeetSharp.Solutions.Library.UnitTest/Problem0001TwoSum.cs
@@ -6,9 +6,9 @@ namespace LeetSharp.Solutions.Library.UnitTest;
 public class Problem0001TwoSum
 {
     [Theory]
-    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
-    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
-    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
     public void BruteForce_ReturnsTarget(int[] nums, int target, int[] expected)
     {
         // Arrange.
@@ -22,9 +22,9 @@ public class Problem0001TwoSum
     }
 
     [Theory]
-    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
-    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
-    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
     public void OnePassDictionary_ReturnsTarget(int[] nums, int target, int[] expected)
     {
         // Arrange.
@@ -38,9 +38,9 @@ public class Problem0001TwoSum
     }
 
     [Theory]
-    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
-    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
-    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
     public void TwoPassDictionary_RetutnsTarget(int[] nums, int target, int[] expected)
     {
         // Arrange.
